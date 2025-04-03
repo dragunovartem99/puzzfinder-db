d67e3f09fe9bffed20b506ca3332ccd32ecb320b
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-03 11:46:00 +0300

    Init

---
+++ create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./automation/themes
+source ./automation/print_header
+source ./automation/prayer
+
+INPUT_CSV="lichess_db_puzzle.csv"
+DB_FILE="puzzfinder.db"
+SCHEMA_FILE="sql/schema.sql"
+INDEXES_FILE="sql/indexes.sql"
+
+if [ ! -f $DB_FILE ] && [ ! -f $INPUT_CSV ]; then
+    print_header "Getting .csv dataset"
+    wget https://database.lichess.org/lichess_db_puzzle.csv.zst
+    zstd -d lichess_db_puzzle.csv.zst && rm lichess_db_puzzle.csv.zst
+fi
+
+print_header "Creating schema"
+. ./automation/create_schema
+
+print_header "Importing data"
+. ./automation/import_data
+
+print_header "Creating indexes"
+. ./automation/create_indexes
+
+print_header "Database is ready!"
