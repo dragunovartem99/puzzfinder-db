052f4adafa43158af4bf8ed0f06d3f636af27c48
Artem Dragunov <dragunovartem99@gmail.com>
2025-07-19 14:33:21 +0300

    Use different header for CLI

--- create.sh
+++ create.sh
@@ -10,12 +10,12 @@ SCHEMA_FILE="sql/schema.sql"
 INDEXES_FILE="sql/indexes.sql"
 
 if [ ! -f $DB_FILE ] && [ ! -f $INPUT_CSV ]; then
-    print_header "Getting .csv dataset"
+    print_header "Getting .csv"
     wget https://database.lichess.org/lichess_db_puzzle.csv.zst
     zstd -d lichess_db_puzzle.csv.zst && rm lichess_db_puzzle.csv.zst
 fi
 
-print_header "Creating schema"
+print_header "Inserting schema"
 . ./automation/create_schema
 
 print_header "Importing data"
@@ -24,4 +24,4 @@ print_header "Importing data"
 print_header "Creating indexes"
 . ./automation/create_indexes
 
-print_header "Database is ready!"
+print_header "Database created"
